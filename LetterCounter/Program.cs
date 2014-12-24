8cc1cf246e2e3d70e7a3c4dbd20a076cc0dd82c7
intrueder <intrueder@gmail.com>
2014-12-24 17:23:38 +0200

    format code; remove unused usings

--- LetterCounter/Program.cs
+++ LetterCounter/Program.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LetterCounter
 {
@@ -14,20 +11,22 @@ namespace LetterCounter
             var input = File.ReadAllText(@"c:\temp\songz.txt");
             var freq = new int[26];
 
-            foreach(var x in input.ToLower().Where(Char.IsLetter))
+            foreach (var x in input.ToLower().Where(Char.IsLetter))
             {
                 freq[x - 'a']++;
             }
-            for (int k=0;k<5;k++)
+
+            for (int k = 0; k < 5; k++)
             {
                 int n = 0;
-                for (int i=1;i<26;i++)
+                for (int i = 1; i < 26; i++)
                 {
                     if (freq[i] > freq[n]) n = i;
                 }
                 Console.Write((char)('a' + n));
                 freq[n] = 0;
             }
+
             Console.ReadKey();
         }
     }
